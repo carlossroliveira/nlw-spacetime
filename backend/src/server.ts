d710047f187215f1a02913b49f6b90cf6fa77986
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-05-16 20:49:05 -0300

    Configuração do prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+const prisma = new PrismaClient();
 
+app.get("/hello", async () => {
+  const users = await prisma.user.findMany();
 
-app.get('/hello', ()=> {
-  return 'Hello world!';
-})
+  return users;
+});
 
 app
   .listen({
