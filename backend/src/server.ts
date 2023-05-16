157d4ba3ef58a2a6fe402359c9b75d233fdeb761
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-05-16 20:20:58 -0300

    Implementação e configuração do backend

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+
+app.get('/hello', ()=> {
+  return 'Hello world!';
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("🚀 HTTP server running on port http://localhost:3333");
+  });
